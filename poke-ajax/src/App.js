44cbdb8c04066e1f98f83d836385b801b57bf6d1
LehmanBrother <chrislehman1991@gmail.com>
2018-10-30 17:25:52 -0500

    Initial structure

---
+++ poke-ajax/src/App.js
@@ -0,0 +1,30 @@
+import React, { Component } from 'react';
+import logo from './logo.svg';
+import './App.css';
+import Login from './Login';
+import MainContainer from './MainContainer';
+
+class App extends Component {
+  constructor(){
+    super();
+    this.state = {
+      logged: false,
+      username: ''
+    }
+  }
+  handleLogin = (username, logged) => {
+    this.setState({
+      username: username,
+      logged: logged
+    });
+  }
+  render() {
+    return (
+      <div className="App">
+        {this.state.logged ? <MainContainer username={this.state.username} /> : <Login handleLogin={this.handleLogin} />}
+      </div>
+    );
+  }
+}
+
+export default App;
